30d7603d35ede6886aeb39d1337f17122abd43a5
xevy <szymontarka4@gmail.com>
2023-11-27 22:59:05 +0100

    zadanie_z_27.11.

---
+++ zadanie_dzisiejsze.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+
+using namespace std;
+
+float oceny[5]; float suma=0, srednia;
+
+int main()
+{
+    int liczby[100000];
+    liczby[0]=100;
+    cout<<"Liczby: "<<endl;
+    cout<<liczby[1];
+    for (int i=0; i<5; i++)
+    {
+        cout<<"cyfra"<<endl;
+        cin>>oceny[i];
+    }
+}
